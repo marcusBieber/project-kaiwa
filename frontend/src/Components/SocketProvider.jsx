36288129b3af3bf9bdff17866e3f24224ee6f46f
Marcus <biebermarcus1@gmail.com>
2025-03-15 20:37:15 +0100

    fix: socket_url angepasst

--- frontend/src/Components/SocketProvider.jsx
+++ frontend/src/Components/SocketProvider.jsx
@@ -7,7 +7,7 @@ import React, {
 } from "react";
 import io from "socket.io-client";
 
-const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:3000' : window.location.origin + '/api';
+const SOCKET_URL = window.location.hostname === 'localhost' ? 'http://localhost:3000' : window.location.origin;
 
 // "Container" für die Socket-Verbindung
 const SocketContext = createContext();
@@ -22,7 +22,7 @@ export const SocketProvider = ({ children, username }) => {
   const socketRef = useRef(null);
   
   useEffect(() => {
-    socketRef.current = io(API_URL, { transports: ["websocket", "polling"] });
+    socketRef.current = io(SOCKET_URL, { transports: ["websocket", "polling"] });
 
     // Verbindung herstellen und im Custom-Hook speichern
     socketRef.current.on("connect", () => {
